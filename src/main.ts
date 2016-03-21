a90e8520f9b63e8250cecb8c60ad074d6cc7bc6d
kai gittens <kai.gittens@gmail.com>
2016-03-21 14:52:18 -0400

    update single Duck instantiation in main.ts (see message) -have the instance be created in an IIFE -run the instance's display method -add/update comments

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,21 @@
 /// <reference path="Duck.ts" />
 /// <reference path="Mallard.ts" />
 
-// Class instances
-let joe: Duck = new Mallard( new Fly.NoFly(), new Quack.MuteQuack(), "Howard" );
+/*
+ * =====================================================================
+ * main.ts
+ *
+ * Code that should be executed in the browser should go here.
+ * =====================================================================
+ */
+
+// Class instances are automaically created on page-load with an IIFE
+(function(){
+  
+  // Create instances of "Mallard"
+  let joe: Duck = new Mallard( new Fly.NoFly(), new Quack.MuteQuack(), "Howard" );
+  
+  // Run the "display()" method for each instance
+  joe.display();
+  
+})();
