bd577a883a68e9a7730bf0d5ad1433de309b36c4
kai gittens <kai.gittens@gmail.com>
2016-03-21 14:51:47 -0400

    fix comment typo

--- src/HelperLibrary.ts
+++ src/HelperLibrary.ts
@@ -13,7 +13,7 @@ module HelperLibrary {
    * setDuckElementId(): a method meant to take the "name" parameter
    * passed to a duck instance and then make lowercase first, then
    * remove any whitespace. "name" is represented by the duck instance's
-   * "element" paramter
+   * "element" parameter
    */
   export function setDuckElementId( element ) {
     return element.toLowerCase().replace( /\s+/g, '' );
