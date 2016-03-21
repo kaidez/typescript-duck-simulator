2c5cf8684a7f2c014de016cb98a26acbc64b46a6
kai gittens <kai.gittens@gmail.com>
2016-03-20 20:21:49 -0400

    update core main.js build

--- main.js
+++ main.js
@@ -1,69 +1,82 @@
-"use strict";
 var __extends = (this && this.__extends) || function (d, b) {
     for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
     function __() { this.constructor = d; }
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
-/*
- * Create a class that lets "fly()" implement some sort of
- * "FlyWithWings" behavior
- */
-var FlyWithWings = (function () {
-    function FlyWithWings() {
-    }
-    FlyWithWings.prototype.fly = function () {
-        return console.log("I'm flying!!");
-    };
-    return FlyWithWings;
-}());
-/*
- * Create a class that lets "fly()" implement some sort of "NoFly"
- * behavior
- */
-var NoFly = (function () {
-    function NoFly() {
-    }
-    NoFly.prototype.fly = function () {
-        return console.log("I can't fly!!");
-    };
-    return NoFly;
-}());
-/*
- * Create a class that lets "quack()" implement some sort of
- * "Quack" behavior
- */
-var Quack = (function () {
-    function Quack() {
-    }
-    Quack.prototype.quack = function () {
-        return console.log("I'm quacking!!");
-    };
-    return Quack;
-}());
-/*
- * Create a class that lets "quack()" implement some sort of
- * "Squeak" behavior
- */
-var Squeak = (function () {
-    function Squeak() {
-    }
-    Squeak.prototype.quack = function () {
-        return console.log("I'm squeaking!!");
-    };
-    return Squeak;
-}());
-/*
- * Create a class that lets "quack()" implement some sort of
- * "MuteQuack" behavior
- */
-var MuteQuack = (function () {
-    function MuteQuack() {
-    }
-    MuteQuack.prototype.quack = function () {
-        return console.log("I don't quack!!");
-    };
-    return MuteQuack;
-}());
+"use strict;";
+var Fly;
+(function (Fly) {
+    /*
+     * Create a class that lets "fly()" implement some sort of
+     * "FlyWithWings" behavior
+     */
+    var FlyWithWings = (function () {
+        function FlyWithWings() {
+        }
+        FlyWithWings.prototype.fly = function () {
+            return console.log("I'm flying!!");
+        };
+        return FlyWithWings;
+    }());
+    Fly.FlyWithWings = FlyWithWings;
+    /*
+     * Create a class that lets "fly()" implement some sort of "NoFly"
+     * behavior
+     */
+    var NoFly = (function () {
+        function NoFly() {
+        }
+        NoFly.prototype.fly = function () {
+            return console.log("I can't fly!!");
+        };
+        return NoFly;
+    }());
+    Fly.NoFly = NoFly;
+})(Fly || (Fly = {}));
+var Quack;
+(function (Quack_1) {
+    /*
+     * Create a class that lets "quack()" implement some sort of
+     * "Quack" behavior
+     */
+    var Quack = (function () {
+        function Quack() {
+        }
+        Quack.prototype.quack = function () {
+            return console.log("I'm quacking!!");
+        };
+        return Quack;
+    }());
+    Quack_1.Quack = Quack;
+    /*
+     * Create a class that lets "quack()" implement some sort of
+     * "Squeak" behavior
+     */
+    var Squeak = (function () {
+        function Squeak() {
+        }
+        Squeak.prototype.quack = function () {
+            return console.log("I'm squeaking!!");
+        };
+        return Squeak;
+    }());
+    Quack_1.Squeak = Squeak;
+    /*
+     * Create a class that lets "quack()" implement some sort of
+     * "MuteQuack" behavior
+     */
+    var MuteQuack = (function () {
+        function MuteQuack() {
+        }
+        MuteQuack.prototype.quack = function () {
+            return console.log("I don't quack!!");
+        };
+        return MuteQuack;
+    }());
+    Quack_1.MuteQuack = MuteQuack;
+})(Quack || (Quack = {}));
+/// <reference path="Fly.ts" />
+/// <reference path="Quack.ts" />
 /*
  * =====================================================================
  * DUCK ABSTRACT CLASSES: a class meant to be inherited from and no more
@@ -110,4 +123,4 @@ var Mallard = (function (_super) {
     return Mallard;
 }(Duck));
 // Class instances
-var joe = new Mallard(new NoFly(), new MuteQuack());
+var joe = new Mallard(new Fly.NoFly(), new Quack.MuteQuack());
