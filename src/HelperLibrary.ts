82d034fad77b2f743b9c32808aac0b9f66f90585
kai gittens <kai.gittens@gmail.com>
2016-03-21 14:34:53 -0400

    complete setDuckElement() method...add comments

--- src/HelperLibrary.ts
+++ src/HelperLibrary.ts
@@ -1,5 +1,20 @@
-module HelperLibrary {
-  export function setDuckElementId() {
+/*
+ * =====================================================================
+ * HelperLibrary: generic methods that help do stuff in other parts of
+ * the code. Classes and Interface signatures should NOT go here!!!!!!!!
+ * =====================================================================
+ */
 
+module HelperLibrary {
+  
+  /*
+   * setDuckElementId(): a method meant to take the "name" parameter
+   * passed to a duck instance and then make lowercase first, then
+   * remove any whitespace. "name" is represented by the method's
+   * "element" paramter
+   */
+  export function setDuckElementId( element ) {
+    return element.toLowerCase().replace(/\s+/g, '');
   }
+  
 }
