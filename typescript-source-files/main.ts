df1edb9b9e8c4e056e7badd99bbaf5cac958e54b
kai gittens <kai.gittens@gmail.com>
2016-03-29 13:07:55 -0400

    remove the IIFE that surrounds the constructor functions, add whitespace

--- typescript-source-files/main.ts
+++ typescript-source-files/main.ts
@@ -11,50 +11,47 @@
  * =====================================================================
  */
 
-// Class instances are automatically created on page-load with an IIFE
-(function(){
 
-  // Create instance of a "Redhead"
-  let stacey: Duck = new Redhead( new Fly.NoFly(), new Quack.MuteQuack(), "Stacy" );
+// Create instance of a "Redhead"
+let stacey: Duck = new Redhead( new Fly.NoFly(), new Quack.MuteQuack(), "Stacy" );
 
-  // Run the "display()" method for the Redhead
+// Run the "display()" method for the Redhead
   stacey.display();
 
 
 // Create instance of a "RubberDuck"
-  let jerrySeinfeld: Duck = new Rubber( new Fly.NoFly(), new Quack.MuteQuack(), "Jerry Seinfeld" );
+let jerrySeinfeld: Duck = new Rubber( new Fly.NoFly(), new Quack.MuteQuack(), "Jerry Seinfeld" );
 
-  // Run the "display()" method for each instance
-  jerrySeinfeld.display();
+// Run the "display()" method for each instance
+jerrySeinfeld.display();
 
-  // Create instance of a "Redhead"
-  let bobby: Duck = new Redhead( new Fly.NoFly(), new Quack.MuteQuack(), "Bobby" );
+// Create instance of a "Redhead"
+let bobby: Duck = new Redhead( new Fly.NoFly(), new Quack.MuteQuack(), "Bobby" );
 
-  // Run the "display()" method for the Redhead
-  bobby.display();
-
-
-
-  // Create instance of a "Mallard"
-  let joe: Duck = new Mallard( new Fly.NoFly(), new Quack.MuteQuack(), "Howard The Duck" );
-
-  // Run the "display()" method for each instance
-  joe.display();
-  joe.swim();
+// Run the "display()" method for the Redhead
+bobby.display();
 
 
 
-  // Create instance of a "RubberDuck"
-  let john: Duck = new Rubber( new Fly.NoFly(), new Quack.MuteQuack(), "John" );
+// Create instance of a "Mallard"
+let joe: Duck = new Mallard( new Fly.NoFly(), new Quack.MuteQuack(), "Howard The Duck" );
 
-  // Run the "display()" method for each instance
-  john.display();
+// Run the "display()" method for each instance
+joe.display();
+joe.swim();
 
 
 
-  // Create instance of a "DecoyDuck"
-  let james: Duck = new Decoy( new Fly.NoFly(), new Quack.MuteQuack(), "James" );
+// Create instance of a "RubberDuck"
+let john: Duck = new Rubber( new Fly.NoFly(), new Quack.MuteQuack(), "John" );
 
-  // Run the "display()" method for each instance
-  james.display();
-})();
+// Run the "display()" method for each instance
+john.display();
+
+
+
+// Create instance of a "DecoyDuck"
+let james: Duck = new Decoy( new Fly.NoFly(), new Quack.MuteQuack(), "James" );
+
+// Run the "display()" method for each instance
+james.display();
