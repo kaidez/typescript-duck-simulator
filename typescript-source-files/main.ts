d6edc21c09a6c7ba0678c7bb25726ef3f160ec79
kai gittens <kai.gittens@gmail.com>
2016-03-30 06:08:01 -0400

    remove & update constructor functions

--- typescript-source-files/main.ts
+++ typescript-source-files/main.ts
@@ -11,46 +11,8 @@
  * =====================================================================
  */
 
-// Create instance of a "Redhead"
-let stacey = new Redhead( new Fly.NoFly(), new Quack.MuteQuack(), "Stacy" );
+// Create instance of a "Decoy"
+let joey = new Decoy( new Fly.NoFly(), new Quack.MuteQuack(), "Joey" );
 
 // Run the "display()" method for the Redhead
-stacey.display();
-
-
-// Create instance of a "RubberDuck"
-let jerrySeinfeld = new Rubber( new Fly.NoFly(), new Quack.MuteQuack(), "Jerry Seinfeld" );
-
-// Run the "display()" method for each instance
-jerrySeinfeld.display();
-
-// Create instance of a "Redhead"
-let bobby = new Redhead( new Fly.NoFly(), new Quack.MuteQuack(), "Bobby" );
-
-// Run the "display()" method for the Redhead
-bobby.display();
-
-
-
-// Create instance of a "Mallard"
-let joe = new Mallard( new Fly.NoFly(), new Quack.MuteQuack(), "Howard The Duck" );
-
-// Run the "display()" method for each instance
-joe.display();
-joe.swim();
-
-
-
-// Create instance of a "RubberDuck"
-let john = new Rubber( new Fly.NoFly(), new Quack.MuteQuack(), "John" );
-
-// Run the "display()" method for each instance
-john.display();
-
-
-
-// Create instance of a "DecoyDuck"
-let james = new Decoy( new Fly.NoFly(), new Quack.MuteQuack(), "James" );
-
-// Run the "display()" method for each instance
-james.display();
+joey.display();
